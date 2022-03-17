b6466cff9d4b209be0d501709c11c0671b4430e3
suhasm-deloitte <101085437+suhasm-deloitte@users.noreply.github.com>
2022-03-17 11:04:03 +0530

    Add files via upload

---
+++ Mini assignment 3/interleave.java
@@ -0,0 +1,30 @@
+public class interleave{
+
+    static boolean isInterleaved (String A, String B, String C)
+    {
+        int i = 0, j = 0, k = 0;
+
+        while (k != C.length())
+        {
+            if (i<A.length()&&A.charAt(i) == C.charAt(k))
+                i++;
+            else if (j<B.length()&&B.charAt(j) == C.charAt(k))
+                j++;
+            else
+                return false;
+            k++;
+        }
+        if (i < A.length() || j < B.length())
+            return false;
+        return true;
+    }
+    public static void main(String []args){
+        String A = "gid";
+        String B = "Hash";
+        String C = "Hgaisdh";
+        if (isInterleaved(A, B, C) == true)
+            System.out.printf("Valid Shuffle", C, A, B);
+        else
+            System.out.printf("Invalid Shuffle", C, A, B);
+    }
+}
