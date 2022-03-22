0bec1fa7e8cfc3801ef0935337e56629065f9bec
suhasm-deloitte <suhasm@deloitte.com>
2022-03-22 13:21:56 +0530

    Update Main.java

--- Main_assignment/Main/src/Main.java
+++ Main_assignment/Main/src/Main.java
@@ -52,14 +52,6 @@ public class Main {
                         password = res[1];
                         valid = res[2];
                     }
-
-//                    if (valid == "valid") {
-//                        System.out.println("Please Login to continue");
-//                        Login log_in = new Login();
-//                        validate = log_in.login_page(username, password);
-//                        break;
-//                    }
-
                     break;
                 }
 
@@ -74,7 +66,6 @@ public class Main {
                     System.out.println("Invalid choice");
             }
 
-//            System.out.println(validate);
             if (validate) {
                 EmpData employee = new EmpData();
 
